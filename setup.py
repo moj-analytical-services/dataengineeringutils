77a9aeecee79969d526a32a759caac255e5c057a
Karik Isichei <karik.isichei@gmail.com>
2017-12-21 14:37:23 +0000

    Adding all the stuff needed for a python package to install via git using pip.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_engineering_utils',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A python package containing functions that help manage our data management processes on AWS',
+    long_description=open('README.md').read(),
+    install_requires=['numpy', 'pandas', 'boto3', 'io'],
+    url='https://github.com/moj-analytical-services/data_engineering_utils',
+    author='Karik Isichei',
+    author_email=''
+)
