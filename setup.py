e75ef86d90fe7947cb5e1f306a07c6e1209081b5
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-12-22 13:35:20 +0000

    fixed problem with package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='data_engineering_utils',
+    name='dataengineeringutils',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
