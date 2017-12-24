bfeb2419a0cac01e9191f4c9305be47f1b11317a
Karik Isichei <karik.isichei@gmail.com>
2017-12-24 19:04:08 +0000

    Moving basic into utils - as putting these into one module makes more sense.

--- dataengineeringutils/utils.py
+++ dataengineeringutils/utils.py
@@ -1,4 +1,5 @@
 import collections
+import json
 
 def dict_merge(dct, merge_dct):
     """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
@@ -15,3 +16,23 @@ def dict_merge(dct, merge_dct):
             dict_merge(dct[k], merge_dct[k])
         else:
             dct[k] = merge_dct[k]
+
+# Read json file
+def read_json(filename) :
+    with open(filename) as json_data:
+        data = json.load(json_data)
+    return data
+
+# Write json file
+def write_json(data, filename) :
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile, indent=4, separators=(',', ': '))
+
+# Read first line of csv and return a list
+def get_csv_header(file_path, convert_to_lower = False) :
+    with open(file_path) as f:
+        line = f.readline()
+        column_names = line.rstrip().split(",")
+        if convert_to_lower :
+            column_names = [c.lower() for c in column_names]
+    return column_names
