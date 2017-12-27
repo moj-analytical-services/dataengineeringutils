3025bfb674f5338251ccc726c8de5f373a1a4cb6
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-12-27 09:46:55 +0000

    added upload and delete tools

--- dataengineeringutils/s3.py
+++ dataengineeringutils/s3.py
@@ -19,4 +19,26 @@ def pd_write_csv_s3(df, path, *args, **kwargs):
     bucket, key = path_to_bucket_key(path)
     csv_buffer = io.StringIO()
     df.to_csv(csv_buffer, *args, **kwargs)
-    s3_resource.Object(bucket, 'df.csv').put(Body=csv_buffer.getvalue())
+    s3_resource.Object(bucket, key).put(Body=csv_buffer.getvalue())
+
+def upload_file_to_s3_from_path(input_path, bucket_name, output_path):
+   s3_client.upload_file(input_path, bucket_name, output_path)
+
+def delete_file_from_s3(bucket_name, key):
+    s3_resource.Object(bucket_name, key).delete()
+
+
+def delete_folder_from_bucket(bucket, folder):
+
+    if '/' in bucket:
+        raise ValueError("You provided bucket name {}, but this has disallowed punctuation in it".format(bucket))
+
+    if folder[-1:] != "/":
+        message = """The folder path you provided doesn't end with a /.
+            Stopping, because you could accidentally end up deleting more than you expected.
+            See https://stackoverflow.com/a/11427712/1779128"""
+        raise ValueError(message)
+
+    bucket = s3_resource.Bucket(bucket)
+
+    bucket.objects.filter(Prefix=folder).delete()
