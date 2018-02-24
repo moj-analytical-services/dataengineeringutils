e0421599660173d433f4ce95ce03006747d09708
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-02-24 11:05:41 +0000

    get a spark schema from the metadata

--- dataengineeringutils/datatypes.py
+++ dataengineeringutils/datatypes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import pkg_resources
 
-def translate_metadata_type_to_type(column_type, target_type):
+def translate_metadata_type_to_type(column_type, target_type="glue"):
 
     io = pkg_resources.resource_stream(__name__, "data/data_type_conversion.csv")
     df = pd.read_csv(io)
@@ -10,6 +10,6 @@ def translate_metadata_type_to_type(column_type, target_type):
     lookup = df.to_dict(orient="index")
 
     try:
-        return lookup[column_type]["glue"]
+        return lookup[column_type][target_type]
     except:
         raise KeyError("You attempted to lookup column type {}, but this cannot be found in data_type_conversion.csv".format(column_type))
